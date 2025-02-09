87722698f320a227047725c9992afff2076f92b2
ahassan200695 <ahassan200695@gmail.com>
2025-02-10 00:36:10 +0200

    Add project files.

---
+++ TrendsValley/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrendsValley.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
