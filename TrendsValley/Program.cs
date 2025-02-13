a0f443cc9b9cf0754e4c123d51307582abbae36c
ahassan200695 <ahassan200695@gmail.com>
2025-02-13 10:47:06 +0200

    feat: refactored project into N-Tier architecture and configured Entity & Identity packages

--- TrendsValley/Program.cs
+++ TrendsValley/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
