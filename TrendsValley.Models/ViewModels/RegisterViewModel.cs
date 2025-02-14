ddc65bf1e78e033089cf6e0533074b4ab7964ac7
ahassan200695 <ahassan200695@gmail.com>
2025-02-14 18:05:47 +0200

    feat: Add RegisterViewModel and LoginViewModel for authentication - Created `RegisterViewModel` to handle user registration data. - Created `LoginViewModel` to manage user login inputs.

---
+++ TrendsValley.Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendsValley.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Fname { get; set; }
+
+        [Required]
+        public string Lname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
