17518286a175210fad18ac892b3bf64154425fed
ahassan200695 <ahassan200695@gmail.com>
2025-02-14 18:08:25 +0200

    feat: Add StandardDefinitions class in Utility project - Created `StandardDefinitions` class to store common constants and definitions. - Centralized standard values to improve maintainability and reusability. - Enhances consistency across the application by reducing hardcoded values.

---
+++ TrendsValley.Utilities/SD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendsValley.Utilities
+{
+    public class SD
+    {
+        public const string User = "User";
+        public const string Admin = "Admin";
+        public const string SuperAdmin = "SuperAdmin";
+
+        public const string Success = "Success";
+        public const string Error = "Error";
+    }
+}
