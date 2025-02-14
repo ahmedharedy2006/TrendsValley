30448a6fcaf811814cc226269024237c6f7c832e
ahassan200695 <ahassan200695@gmail.com>
2025-02-14 17:56:05 +0200

    feat: Add AuthController with GET methods for SignIn and Register views - Implemented `AuthController` to handle authentication-related views. - Added `SignIn` and `Register` GET methods returning corresponding views.

---
+++ TrendsValley/Areas/Customer/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TrendsValley.Models.Models;
+
+namespace TrendsValley.Areas.Customer.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AuthController(SignInManager<AppUser> signInManager,RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+        
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
