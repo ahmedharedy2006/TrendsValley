f5ab4e41f975ddfacdd130690bae74323057320f
MohamedGasser15 <madagasser15@gmail.com>
2025-02-18 16:29:37 +0200

    feat(category): add CategoryController with CRUD operations

--- TrendsValley/Areas/Admin/Controllers/CategoryController.cs
+++ TrendsValley/Areas/Admin/Controllers/CategoryController.cs
@@ -1,12 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TrendsValley.DataAccess.Data;
+using TrendsValley.Models.Models;
 
 namespace TrendsValley.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objList = _db.Categories.ToList();
+            return View(objList);
+        }
+        public IActionResult Upsert(int? id)
+        {
+            Category obj = new();
+            if(id == null || id == 0)
+            {
+                //Create
+                return View(obj);
+            }
+            //edit
+            obj = _db.Categories.FirstOrDefault(u => u.Category_id == id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Category obj)
+        {
+            if (obj.Category_id == 0)
+            {
+                await _db.Categories.AddAsync(obj);
+            }
+            else
+            {
+                _db.Categories.Update(obj);
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        //Remove
+        public IActionResult Delete(int id)
+        {
+            Category obj = new();
+             obj = _db.Categories.FirstOrDefault(c => c.Category_id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.Categories.Remove(obj);
+            }
+             _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
