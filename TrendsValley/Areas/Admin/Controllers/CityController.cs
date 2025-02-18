5c904f5bf85010b74be5f061fd63b3a14538c284
MohamedGasser15 <madagasser15@gmail.com>
2025-02-18 22:19:12 +0200

    feat(city): implement City Controller with CRUD operations

---
+++ TrendsValley/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,73 @@
+﻿using BooksMine.Models;
+using Microsoft.AspNetCore.Mvc;
+using TrendsValley.DataAccess.Data;
+using TrendsValley.Models.Models;
+
+namespace TrendsValley.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CityController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<City> objList = _db.cities.ToList();
+            return View(objList);
+        }
+
+
+        public IActionResult Upsert(int? id)
+        {
+            City obj = new();
+            if (id == null || id == 0)
+            {
+                return View(obj);
+            }
+            obj = _db.cities.FirstOrDefault(u => u.City_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(City obj)
+        {
+            if (obj.City_Id == null)
+            {
+                await _db.cities.AddAsync(obj);
+            }
+            else
+            {
+                _db.cities.Update(obj);
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        //Remove
+        public IActionResult Delete(int id)
+        {
+            City obj =new();
+            obj = _db.cities.FirstOrDefault(u => u.City_Id == id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.cities.Remove(obj);
+            }
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
