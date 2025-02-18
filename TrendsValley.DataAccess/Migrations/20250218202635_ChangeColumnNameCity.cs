2c567e85b32a299bb2d92b11eb6782407ee3a0fa
MohamedGasser15 <madagasser15@gmail.com>
2025-02-18 22:32:53 +0200

    feat(index): Create and implement index view for initial rendering

---
+++ TrendsValley.DataAccess/Migrations/20250218202635_ChangeColumnNameCity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrendsValley.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeColumnNameCity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "cities",
+                newName: "City_Name");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "cities",
+                newName: "City_Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "City_Name",
+                table: "cities",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "City_Id",
+                table: "cities",
+                newName: "Id");
+        }
+    }
+}
--- TrendsValley.Models/Models/City.cs
+++ TrendsValley.Models/Models/City.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+
 namespace BooksMine.Models
 {
     public class City
