179ee9305c2ddc973a77c16960e809c388cecd7a
ahassan200695 <ahassan200695@gmail.com>
2025-03-14 04:44:46 +0200

    feat: add Cart ViewModel

---
+++ TrendsValley.Models/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrendsValley.Models.Models;
+
+namespace TrendsValley.Models.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public IEnumerable<ShoppingCart> ListCart { get; set; }
+
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
