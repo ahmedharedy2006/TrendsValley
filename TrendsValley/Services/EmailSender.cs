ef6ce3f4397b21a9f5b6d5b2b07c0b690d386fd4
MohamedGasser15 <madagasser15@gmail.com>
2025-03-17 14:27:20 +0200

    feat: add EmailSender for email functionality
    - Added EmailSender.cs to handle email sending via SendGrid API
    - Configured email service to integrate with application authentication and notifications

---
+++ TrendsValley/Services/EmailSender.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace Clothes_Store.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        //Sendgrid Setup
+        public string SendGridKey { get; set; }
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridKey = _config.GetValue<string>("SendGrid:SecretKey");
+        }
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+            {
+            var client = new SendGridClient(SendGridKey);
+            var from_email = new EmailAddress("carastore871@gmail.com", "Cara_Store");
+
+            var to_email = new EmailAddress(email);
+
+            var msg = MailHelper.CreateSingleEmail(from_email, to_email, subject, "", htmlMessage);
+            return client.SendEmailAsync(msg);
+        }
+    }
+}
