4e400dfad8b436275a920db82f1676847208e157
MohamedGasser15 <madagasser15@gmail.com>
2025-03-17 14:31:15 +0200

    feat: add ResetPasswordViewModel, ForgetPasswordViewModel, and VerifyCodeViewModel
    - Added ResetPasswordViewModel for handling password reset requests
    - Added ForgetPasswordViewModel to manage password recovery
    - Implemented VerifyCodeViewModel to handle two-factor authentication or email verification codes
    - These ViewModels improve the authentication and account recovery process

---
+++ TrendsValley.Models/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendsValley.Models.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ TrendsValley.Models/ViewModels/VerifyCodeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendsValley.Models.ViewModels
+{
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+    }
+}
