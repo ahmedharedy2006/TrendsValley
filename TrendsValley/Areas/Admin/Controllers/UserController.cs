8746cb64bda5a5652a44738ca86b974bcb6cd97e
MohamedGasser15 <madagasser15@gmail.com>
2025-03-19 02:42:25 +0200

    feat: add UserController
    Implemented UserController to manage user-related operations such as retrieval, updates, and profile management.

---
+++ TrendsValley/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TrendsValley.DataAccess.Data;
+using TrendsValley.Models.Models;
+
+namespace TrendsValley.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly AppDbContext _db;
+
+        public UserController(UserManager<AppUser> userManager, AppDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
