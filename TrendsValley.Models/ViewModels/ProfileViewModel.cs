4c51f7a49c7280410cda5da4780dbda4b155ee6b
MohamedGasser15 <madagasser15@gmail.com>
2025-03-24 00:00:43 +0200

    feat: add ProfileViewModel
    - Created ProfileViewModel class to represent profile-related data
    - Added necessary properties (e.g., FirstName, Email, Address, etc.)
    - Configured validation attributes for input fields (if applicable)
    - Updated relevant views to use ProfileViewModel for data binding

---
+++ TrendsValley.Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendsValley.Models.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "Current City")]
+        public string CurrentCity { get; set; }
+
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        public List<SelectListItem> GenderOptions { get; set; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "Male", Text = "Male" },
+            new SelectListItem { Value = "Female", Text = "Female" }
+        };
+
+        [Display(Name = "Profile Image")]
+        public string ProfileImageUrl { get; set; }
+    }
+}
