c6d898a04556a99db76b3d227a33a887bde611ce
MohamedGasser15 <madagasser15@gmail.com>
2025-03-24 00:10:03 +0200

    feat: add ProfileController
    - Created ProfileController to handle profile-related actions
    - Added actions for viewing, editing, and updating user profiles
    - Implemented logic to interact with ProfileViewModel and ApplicationUser
    - Configured routing for profile-related endpoints

---
+++ TrendsValley/Areas/Customer/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using TrendsValley.DataAccess.Data;
+using TrendsValley.Models.Models;
+using TrendsValley.Models.ViewModels;
+
+namespace TrendsValley.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly AppDbContext _db;
+
+        public ProfileController(UserManager<AppUser> userManager,
+                                  AppDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
