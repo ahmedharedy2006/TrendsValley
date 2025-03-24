1162c07369a268296d8824b658c05daa0c8f351d
ahassan200695 <ahassan200695@gmail.com>
2025-03-24 13:01:16 +0200

    fix: add ProductViewModel to ShoppingCart model

--- TrendsValley.Models/Models/ShoppingCart.cs
+++ TrendsValley.Models/Models/ShoppingCart.cs
@@ -5,20 +5,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrendsValley.Models.ViewModels;
 
 namespace TrendsValley.Models.Models
 {
     public class ShoppingCart
     {
         public int Id { get; set; }
-        [Range(1, 1000, ErrorMessage = "Please Enter A Value Between 1 And 1000")]
-
-        public int Quantity { get; set; }
-
+       
         [ForeignKey("Product")]
         public int ProductId { get; set; }
 
         public Product Product { get; set; }
+
+        public ProductDetailsViewModel ProductDetailsViewModel { get; set; }
 
         [ForeignKey("appUser")]
 
