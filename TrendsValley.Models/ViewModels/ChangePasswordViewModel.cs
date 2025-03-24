c9a90bbe86caa800ea6d3a76efb093e577f4f788
MohamedGasser15 <madagasser15@gmail.com>
2025-03-25 01:32:26 +0200

    feat: add ChangePasswordViewModel
    - Created viewmodel with properties:
      - CurrentPassword [Required]
      - NewPassword [Required, MinLength(8)]
      - ConfirmPassword [Compare("NewPassword")]
    - Added password complexity validation
    - Prepared for password change form integration

---
+++ TrendsValley.Models/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendsValley.Models.ViewModels
+{
+        public class ChangePasswordViewModel
+        {
+            [Required(ErrorMessage = "Current password is required")]
+            [DataType(DataType.Password)]
+            [Display(Name = "Current Password")]
+            public string CurrentPassword { get; set; }
+
+            [Required(ErrorMessage = "New password is required")]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
+            [DataType(DataType.Password)]
+            [Display(Name = "New Password")]
+            [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",
+                ErrorMessage = "Password must contain at least one uppercase, one lowercase, one number, and one special character")]
+            public string NewPassword { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm New Password")]
+            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+}
