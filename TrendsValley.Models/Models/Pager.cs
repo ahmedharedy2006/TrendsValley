c527136bd9d9bf634750aa7fb4d3c90bb0586ccb
ahassan200695 <ahassan200695@gmail.com>
2025-03-30 16:37:56 +0200

    feat: add Pager model class for pagination

---
+++ TrendsValley.Models/Models/Pager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendsValley.Models.Models
+{
+    public class Pager
+    {
+        public int CurrentPage { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalItems { get; private set; }
+        public int TotalPages { get; private set; }
+        public int StartPage { get; private set; }
+        public int EndPage { get; private set; }
+
+
+        public Pager()
+        {
+
+        }
+
+        public Pager(int totalItems, int page, int pageSize = 10)
+        {
+            int totalPages = (int)Math.Ceiling((decimal)totalItems / (decimal)pageSize);
+            int currentPage = page != null ? (int)page : 1;
+            int startPage = currentPage - 5;
+            int endPage = currentPage + 4;
+
+            if (startPage <= 0)
+            {
+                endPage -= (startPage - 1);
+                startPage = 1;
+            }
+            if (endPage > totalPages)
+            {
+                endPage = totalPages;
+                if (endPage > 10)
+                {
+                    startPage = endPage - 9;
+                }
+            }
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalItems = totalItems;
+            TotalPages = totalPages;
+            StartPage = startPage;
+            EndPage = endPage;
+        }
+    }
+}
