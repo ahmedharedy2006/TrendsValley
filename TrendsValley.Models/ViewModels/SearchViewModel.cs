558363c09f617d2fed098fc943212f8c8966e0db
MohamedGasser15 <madagasser15@gmail.com>
2025-03-30 07:43:25 +0200

    feat: add SearchViewModel for query handling
    - Created SearchViewModel with:
      • SearchTerm (string)
      • Results (List<T>)
      • Pagination properties
      • Filters (status/date ranges)
    - Supports:
      • Client-side validation
      • Typeahead suggestions
      • Multi-criteria searches

---
+++ TrendsValley.Models/ViewModels/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrendsValley.Models.Models;
+
+namespace TrendsValley.Models.ViewModels
+{
+    public class SearchViewModel
+    {
+        public string SearchTerm { get; set; }
+        public List<Product> Results { get; set; }
+    }
+}
