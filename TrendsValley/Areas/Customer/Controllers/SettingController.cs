20ea2f36a7b1585693182efe075c62b0aec5677a
MohamedGasser15 <madagasser15@gmail.com>
2025-03-30 08:36:09 +0200

    feat: add SettingsController
    - Basic CRUD actions
    - Uses _SettingsLayout
    - Routes configured

---
+++ TrendsValley/Areas/Customer/Controllers/SettingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrendsValley.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+
+    public class SettingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
