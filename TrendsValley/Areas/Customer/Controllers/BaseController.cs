e0c91df26d036fc1ef513b2a0dc0d6c7ab5dee44
MohamedGasser15 <madagasser15@gmail.com>
2025-04-01 13:31:31 +0200

    feat: add BaseController for shared logic
    - Inherits from Controller
    - Contains:
      • Common error handling
      • User context shortcuts
      • Audit logging helpers
    - All other controllers inherit from it

---
+++ TrendsValley/Areas/Customer/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Globalization;
+
+namespace TrendsValley.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class BaseController : Controller
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (HttpContext.Session != null)
+            {
+                string lang = HttpContext.Session.GetString("lang") ?? "en";
+
+                CultureInfo culture = new CultureInfo(lang);
+                Thread.CurrentThread.CurrentUICulture = culture;
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(lang);
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
