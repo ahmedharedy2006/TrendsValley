c326aaefe1014c947905a2f80dcc41af97e63ea0
MohamedGasser15 <madagasser15@gmail.com>
2025-04-01 13:32:31 +0200

    feat: add language change in SettingsController
    - Added POST ChangeLanguage:
      • Updates user's PreferredLanguage
      • Saves to database
      • Returns JSON success/error
    - Validates supported languages

--- TrendsValley/Areas/Customer/Controllers/SettingController.cs
+++ TrendsValley/Areas/Customer/Controllers/SettingController.cs
@@ -1,14 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TrendsValley.Models.Models;
 
 namespace TrendsValley.Areas.Customer.Controllers
 {
     [Area("Customer")]
 
-    public class SettingController : Controller
+    public class SettingController : BaseController
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingController(UserManager<AppUser> userManager)
         {
-            return View();
+            _userManager = userManager;
         }
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            return View(user); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeLanguage(string PreferredLanguage)
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user != null)
+            {
+                user.PreferredLanguage = PreferredLanguage;
+                await _userManager.UpdateAsync(user);
+
+                if (HttpContext.Session != null)
+                {
+                    HttpContext.Session.SetString("lang", PreferredLanguage);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
